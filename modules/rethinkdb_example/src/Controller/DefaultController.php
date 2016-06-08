86c534f45c3ec9b043495421f0ef4623eb16374c
Roy Segall <roy.segall@gizra.com>
2016-06-08 20:32:01 +0300

    Add gitignore and the base route for testing.

---
+++ modules/rethinkdb_example/src/Controller/DefaultController.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\rethinkdb_example\Controller\DefaultController.
+ */
+
+namespace Drupal\rethinkdb_example\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\rethinkdb_example\Entity\RethinkMessages;
+
+/**
+ * Class DefaultController.
+ *
+ * @package Drupal\rethinkdb_example\Controller
+ */
+class DefaultController extends ControllerBase {
+  /**
+   * Index.
+   *
+   * @return string
+   *   Return Hello string.
+   */
+  public function index() {
+
+    // Get the first message for testing.
+    $message = RethinkMessages::load(1);
+
+    if (!$message) {
+      RethinkMessages::create([
+        'title' => 'foo',
+        'uid' => 1,
+      ])->save();
+    }
+
+    return [
+      '#type' => 'markup',
+      '#markup' => $message->get('title')->value,
+    ];
+  }
+
+}
---
+++ modules/rethinkdb_example/src/Entity/RethinkMessages.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * @contains \Drupal\rethinkdb_example\Entity\RethinkMessages.
+ */
+
+namespace Drupal\rethinkdb_example\Entity;
+
+use Drupal\Core\Entity\ContentEntityBase;
+use Drupal\Core\Entity\EntityTypeInterface;
+use Drupal\Core\Field\BaseFieldDefinition;
+
+/**
+ * Defines the node entity class.
+ *
+ * @ContentEntityType(
+ *   id = "rethinkdb_message",
+ *   label = @Translation("RethinkDB messages"),
+ *   base_table = "rethinkdb_messages",
+ *   translatable = TRUE,
+ *   entity_keys = {
+ *     "id" = "mid",
+ *     "label" = "title",
+ *     "langcode" = "langcode",
+ *     "uuid" = "uuid",
+ *     "status" = "status",
+ *     "uid" = "uid",
+ *   },
+ * )
+ */
+class RethinkMessages extends ContentEntityBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function baseFieldDefinitions(EntityTypeInterface $entity_type) {
+    $fields = parent::baseFieldDefinitions($entity_type);
+
+    $fields['title'] = BaseFieldDefinition::create('string')
+      ->setLabel(t('Title'))
+      ->setRequired(TRUE)
+      ->setTranslatable(TRUE)
+      ->setRevisionable(TRUE)
+      ->setSetting('max_length', 255)
+      ->setDisplayOptions('view', array(
+        'label' => 'hidden',
+        'type' => 'string',
+        'weight' => -5,
+      ))
+      ->setDisplayOptions('form', array(
+        'type' => 'string_textfield',
+        'weight' => -5,
+      ))
+      ->setDisplayConfigurable('form', TRUE);
+
+    return $fields;
+  }
+
+}
+
+
