25e248e73e9ae555a3ed8049ef7a036a16dc1f57
Roy Segall <roy.segall@gizra.com>
2016-06-12 23:05:55 +0300

    Few menu rating override.

--- modules/rethinkdb_example/src/Controller/DefaultController.php
+++ modules/rethinkdb_example/src/Controller/DefaultController.php
@@ -47,6 +47,7 @@ class DefaultController extends ControllerBase {
   public function index() {
 
     $rethink_storage = $this->tempStore->get('rethinkdb');
+    $params = [];
 
     if ($document_id = $rethink_storage->get('document_id')) {
       // Since we displaying only the created row we need to delete it once we
@@ -55,12 +56,13 @@ class DefaultController extends ControllerBase {
       $text = $this->t('You entered a new record to the DB - @record', ['@record' => $document_id]);
     }
     else {
-      $params = [
-        '@url' => Url::fromRoute('rethinkdb_example.message_create_form')->toString(),
-      ];
-
-      $text = $this->t('In order to show the last message you need to <a href="@url">create it</a>', $params);
+      $params['@url'] = Url::fromRoute('rethinkdb_example.message_create_form')->toString();
+      $text = $this->t('In order to show the last message you need to <a href="@url">create it</a>.', $params);
     }
+
+    $text .= '<br />';
+    $params['@url'] = Url::fromRoute('rethinkdb_example.message_list')->toString();
+    $text .= $this->t('You can also watch the <a href="@url">other messages</a>.', $params);
 
     return [
       '#type' => 'markup',
