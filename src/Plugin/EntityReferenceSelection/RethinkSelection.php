2a14c1e8275df9c53f83c5310f7c423cfd8e5872
Roy Segall <roy.segall@gizra.com>
2016-06-17 00:20:09 +0300

    Start working on the reference field.

---
+++ src/Plugin/EntityReferenceSelection/RethinkSelection.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\rethinkdb\Plugin\EntityReferenceSelection\RethinkSelection.
+ */
+
+namespace Drupal\rethinkdb\Plugin\EntityReferenceSelection;
+
+use Drupal\Core\Entity\Plugin\EntityReferenceSelection\DefaultSelection;
+
+/**
+ * @EntityReferenceSelection(
+ *   id = "default:rethinkdb_default",
+ *   label = @Translation("RethinkDB selection"),
+ *   group = "default",
+ *   weight = 1
+ * )
+ */
+class RethinkSelection extends DefaultSelection {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function buildEntityQuery($match = NULL, $match_operator = 'CONTAINS') {
+
+    $query = \Drupal::entityQuery($this->configuration['target_type']);
+
+    return $query;
+  }
+
+}
