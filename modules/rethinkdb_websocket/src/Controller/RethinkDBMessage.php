d18e1a2f376d39cca58e0b336b0d045502edc04f
Roy Segall <roy.segall@gizra.com>
2016-06-20 23:56:52 +0300

    Adding live chat page.

---
+++ modules/rethinkdb_websocket/src/Controller/RethinkDBMessage.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @file
+ * Contains Drupal\rethinkdb_websocket\Controller\RethinkDBMessage.
+ */
+
+namespace Drupal\rethinkdb_websocket\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+/**
+ * Class RethinkDBMessage.
+ *
+ * @package Drupal\rethinkdb_websocket\Controller
+ */
+class RethinkDBMessage extends ControllerBase {
+
+  /**
+   * Livechat.
+   *
+   * @return string
+   *   Return Hello string.
+   */
+  public function LiveChat() {
+    return [
+        '#type' => 'markup',
+        '#markup' => $this->t('Implement method: LiveChat')
+    ];
+  }
+
+}
