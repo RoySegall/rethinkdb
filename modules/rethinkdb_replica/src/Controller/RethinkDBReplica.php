f4c1dc0c4a743a85f3aa01f86efa3cb1358a263a
RoySegall <roy.segall@gizra.com>
2016-06-24 14:11:54 +0200

    Adding UI for replicate entities.

--- modules/rethinkdb_replica/src/Controller/RethinkDBReplica.php
+++ modules/rethinkdb_replica/src/Controller/RethinkDBReplica.php
@@ -23,9 +23,29 @@ class RethinkDBReplica extends ControllerBase {
    *   Manageable replica list of entities.
    */
   public function replicaList() {
+
+    $replicas = RethinkReplicaList::loadMultiple();
+
+    $rows = [];
+    foreach (\Drupal::entityTypeManager()->getDefinitions() as $entity_type_id => $entity_type_info) {
+      if (in_array($entity_type_id, array_keys($replicas))) {
+        $operation = $this->t('Cloned');
+      }
+      else {
+        $params = ['entity' => $entity_type_id];
+        $operation = Link::createFromRoute($this->t('Create a replica table'), 'rethinkdb_replica.rethinkdb_replica_create', $params)->toString();
+      }
+
+      $rows[] = [
+        $entity_type_info->getLabel(),
+        $operation,
+      ];
+    }
+
     return [
-      '#type' => 'item_list',
-      '#markup' => Link::createFromRoute('Node', 'rethinkdb_replica.rethinkdb_replica_create', ['entity' => 'node'])->toString(),
+      '#theme' => 'table',
+      '#header' => [$this->t('Entity type'), $this->t('Operations')],
+      '#rows' => $rows,
     ];
   }
 
@@ -41,7 +61,11 @@ class RethinkDBReplica extends ControllerBase {
     /** @var RethinkDB $rethink */
     $rethink = \Drupal::service('rethinkdb');
     $rethink->tableCreate($entity . '_replica');
-    RethinkReplicaList::create(['id' => 'node'])->save();
+    RethinkReplicaList::create(['id' => $entity])->save();
+
+    $params = ['@name' => \Drupal::entityTypeManager()->getDefinition($entity)->getLabel()];
+    drupal_set_message($this->t('A replica table for @name has created.', $params));
+
     return new RedirectResponse(Url::fromRoute('rethinkdb_replica.rethinkdb_replica_list')->toString());
   }
 
